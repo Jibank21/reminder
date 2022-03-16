7cac8ba3d22801d91a9502700650e0e57d1c31aa
jibank21 <jeevan.k21.jk@gmail.com>
2022-03-16 23:17:27 +1100

    appication running instance

---
+++ src/Components/TodoList.js
@@ -0,0 +1,19 @@
+import React from 'react'
+//importing components
+import Todo from './Todo' 
+
+const TodoList=({todos, setTodos, filterTodos})=> {
+    return (
+        <div className="todo-container">
+            <ul className="todo-list">
+               {filterTodos.map((todo)=>(
+                   <Todo key={todo.id} text={todo.text}
+                   todo={todo}
+                   todos={todos} setTodos={setTodos}/> 
+               ))}
+            </ul>
+        </div>
+    )
+}
+
+export default TodoList
